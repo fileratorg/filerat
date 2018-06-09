a95966269e2c73d22fadcc7608b5ac7bbf05c5b7
João Guerreiro <joaofcguerreiro@gmail.com>
2018-06-09 19:44:34 +0100

    Add Gin package and setup basic server

--- ginserver/main.go
+++ ginserver/main.go
@@ -1,7 +1,13 @@
 package main
 
-import "fmt"
+import "github.com/gin-gonic/gin"
 
 func main() {
-	fmt.Printf("hello, amigos!\n")
+	r := gin.Default()
+	r.GET("/hello", func(c *gin.Context) {
+		c.JSON(200, gin.H{
+			"message": "world",
+		})
+	})
+	r.Run() // listen and serve on 0.0.0.0:8080
 }
