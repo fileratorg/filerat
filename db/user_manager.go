9e7941d1a2c9441e64a63c2c7fa926576d950511
Luis Sena <luis.sena87@gmail.com>
2018-06-10 11:59:46 +0100

    delegate responsibility to upper layer

--- db/user_manager.go
+++ db/user_manager.go
@@ -3,8 +3,7 @@ package db
 import (
 	"github.com/fileratorg/filerat/db/models"
 	"github.com/fileratorg/filerat"
-	"github.com/fileratorg/filerat/utils"
-	"github.com/fileratorg/filerat/db/neo4j_driver"
+		"github.com/fileratorg/filerat/db/neo4j_driver"
 	"time"
 	"github.com/satori/go.uuid"
 )
@@ -13,17 +12,18 @@ func (conn *DbConnector) SaveUser(user *models.AuthUser) (*models.AuthUser){
 	conn.Open(filerat.BoltPath, filerat.Port)
 	defer conn.Close()
 
-	user.Password = utils.GetPasswordHash(user.Password)
 	now := time.Now()
 	if user.Model == nil{
 		user.Model = new(neo4j_driver.Model)
-	}
-	user.Model.CreatedAt = now
-	user.Model.UpdatedAt = now
-	if user.Model.UniqueId == uuid.Nil{
+		user.Model.CreatedAt = now
+		user.Model.UpdatedAt = now
 		var uniqueId, _ = uuid.NewV4()
 		user.Model.UniqueId = uniqueId
 	}
+	//if user.Model.UniqueId == uuid.Nil{
+	//	var uniqueId, _ = uuid.NewV4()
+	//	user.Model.UniqueId = uniqueId
+	//}
 	conn.Save(user)
 	return user
 }
