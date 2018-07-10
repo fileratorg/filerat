57281518f87cccbdf014547a55efcf5070f778a8
Luis Sena <luis.sena87@gmail.com>
2018-07-10 20:32:13 +0100

    auto save relations

---
+++ db/rat_file_manager.go
@@ -0,0 +1,82 @@
+package db
+
+import (
+	"github.com/fileratorg/filerat/db/models"
+	"github.com/fileratorg/filerat"
+		"github.com/fileratorg/filerat/db/neo4j_driver"
+	"time"
+	"github.com/satori/go.uuid"
+)
+
+
+func (conn *DbConnector) SaveFolder(obj *models.RatFolder) (*models.RatFolder){
+	conn.Open(filerat.BoltPath, filerat.Port)
+	defer conn.Close()
+
+	now := time.Now()
+	if obj.Model == nil{
+		obj.Model = new(neo4j_driver.Model)
+		obj.Model.CreatedAt = now
+		obj.Model.UpdatedAt = now
+		var uniqueId, _ = uuid.NewV4()
+		obj.Model.UniqueId = uniqueId
+	}
+	conn.Save(obj)
+	return obj
+}
+
+
+func (conn *DbConnector) GetFolder(uniqueId uuid.UUID) (models.RatFolder){
+	conn.Open(filerat.BoltPath, filerat.Port)
+	defer conn.Close()
+	obj := models.RatFolder{}
+	obj.Model = new(neo4j_driver.Model)
+	conn.Get(&obj, uniqueId)
+
+	return obj
+}
+
+
+func (conn *DbConnector) DeleteFolder(obj *models.RatFolder, soft bool) bool {
+	conn.Open(filerat.BoltPath, filerat.Port)
+	defer conn.Close()
+
+	conn.Delete(&obj, obj.UniqueId, soft)
+	return true
+}
+
+func (conn *DbConnector) SaveFile(obj *models.RatFile) (*models.RatFile){
+	conn.Open(filerat.BoltPath, filerat.Port)
+	defer conn.Close()
+
+	now := time.Now()
+	if obj.Model == nil{
+		obj.Model = new(neo4j_driver.Model)
+		obj.Model.CreatedAt = now
+		obj.Model.UpdatedAt = now
+		var uniqueId, _ = uuid.NewV4()
+		obj.Model.UniqueId = uniqueId
+	}
+	conn.Save(obj)
+	return obj
+}
+
+
+func (conn *DbConnector) GetFile(uniqueId uuid.UUID) (models.RatFile){
+	conn.Open(filerat.BoltPath, filerat.Port)
+	defer conn.Close()
+	obj := models.RatFile{}
+	obj.Model = new(neo4j_driver.Model)
+	conn.Get(&obj, uniqueId)
+
+	return obj
+}
+
+
+func (conn *DbConnector) DeleteFile(obj *models.RatFile, soft bool) bool {
+	conn.Open(filerat.BoltPath, filerat.Port)
+	defer conn.Close()
+
+	conn.Delete(&obj, obj.UniqueId, soft)
+	return true
+}
---
+++ db/rat_org_manager.go
@@ -0,0 +1,49 @@
+package db
+
+import (
+	"github.com/fileratorg/filerat/db/models"
+	"github.com/fileratorg/filerat"
+		"github.com/fileratorg/filerat/db/neo4j_driver"
+	"time"
+	"github.com/satori/go.uuid"
+)
+
+func (conn *DbConnector) SaveRatOrg(obj *models.RatOrg) (*models.RatOrg){
+	conn.Open(filerat.BoltPath, filerat.Port)
+	defer conn.Close()
+
+	now := time.Now()
+	if obj.Model == nil{
+		obj.Model = new(neo4j_driver.Model)
+		obj.Model.CreatedAt = now
+		obj.Model.UpdatedAt = now
+		var uniqueId, _ = uuid.NewV4()
+		obj.Model.UniqueId = uniqueId
+	}
+	//if user.Model.UniqueId == uuid.Nil{
+	//	var uniqueId, _ = uuid.NewV4()
+	//	user.Model.UniqueId = uniqueId
+	//}
+	conn.Save(obj)
+	return obj
+}
+
+
+func (conn *DbConnector) GetRatOrg(uniqueId uuid.UUID) (models.RatOrg){
+	conn.Open(filerat.BoltPath, filerat.Port)
+	defer conn.Close()
+	obj := models.RatOrg{}
+	obj.Model = new(neo4j_driver.Model)
+	conn.Get(&obj, uniqueId)
+
+	return obj
+}
+
+
+func (conn *DbConnector) DeleteRatOrg(obj *models.RatOrg, soft bool) bool {
+	conn.Open(filerat.BoltPath, filerat.Port)
+	defer conn.Close()
+
+	conn.Delete(&obj, obj.UniqueId, soft)
+	return true
+}
